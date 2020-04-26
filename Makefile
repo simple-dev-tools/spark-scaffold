76aca335935ff6e4bfeb0f016fe6d94311640af7
Zhong Dai <ZhongDai.au@gmail.com>
2020-04-26 17:08:35 +1000

    updated the Makefile to enable execution of the example job

--- Makefile
+++ Makefile
@@ -16,7 +16,7 @@ build-spark-jar: ## fat jar for cluster submission without provided dependencies
 	sbt 'pipeline/assembly'
 
 submit-job: build-spark-jar ## submit the example job to local
-	$(SPARK_SUBMIT) --master local --class $(MAIN_CLASS) $(JAR)  -e local -d false --p_dt 2020-03-09 --p_int 3 --p_string col3 --csv $(CSV_PATH)
+	$(SPARK_SUBMIT) --master local --class $(MAIN_CLASS) $(JAR) -e local -d false --csv $(CSV_PATH) -l 3
 
 clean: ## remove metastore_db and spark-warehouse folders
 	find . -path '*/metastore_db/*' -delete
--- pipeline/src/test/scala/com/log2/scaffold/execution/CsvJobTest.scala
+++ pipeline/src/test/scala/com/log2/scaffold/execution/CsvJobTest.scala
@@ -2,16 +2,10 @@ package com.log2.scaffold.execution
 
 import com.log2.scaffold.test.ContextSuite
 import org.scalatest.{FlatSpec, Matchers}
-import java.sql.Date
-import java.text.SimpleDateFormat
 import java.util.NoSuchElementException
 
-import com.log2.scaffold.constants.Values.DefaultDateFormat
 
 class CsvJobTest extends FlatSpec with Matchers with ContextSuite {
-
-  val exampleDtStr = "2020-02-02"
-  val exampleDt = new Date(new SimpleDateFormat(DefaultDateFormat).parse(exampleDtStr).getTime())
 
   val csvPath = "data/example.csv"
 
@@ -19,32 +13,26 @@ class CsvJobTest extends FlatSpec with Matchers with ContextSuite {
     val params = CsvJob.buildParams(Array(
       "--env=local",
       "--debug=true",
-      s"--p_dt=${exampleDtStr}",
-      "--p_int=3",
-      "--p_string=abc",
-      s"--csv=${csvPath}"
+      s"--csv=${csvPath}",
+      "-l=3"
     ))
 
     params shouldBe
       CsvJobParam(
         env = "local",
         debug = true,
-        paramDate = exampleDt,
-        paramInt = 3,
-        paramString = "abc",
-        csvFilePath = csvPath
+        csvFilePath = csvPath,
+        showLines = 3
       )
   }
 
-  it should "throw exception because of the wrong Date format" in {
+  it should "throw exception because of lines < 1" in {
     intercept[NoSuchElementException] {
       CsvJob.buildParams(Array(
         "--env=local",
         "--debug=true",
-        s"--p_dt=20200202",
-        "--p_int=3",
-        "--p_string=abc",
-        s"--csv=${csvPath}"
+        s"--csv=${csvPath}",
+        "-l=0"
       ))
     }
   }
@@ -54,10 +42,8 @@ class CsvJobTest extends FlatSpec with Matchers with ContextSuite {
       CsvJob.buildParams(Array(
         "--env=local",
         "--debug=true",
-        s"--p_dt=20200202",
-        "--p_int=3",
-        "--p_string=abc",
-        s"--csv=some/path/text.txt"
+        s"--csv=some/path/text.txt",
+        "-l=3"
       ))
     }
   }
